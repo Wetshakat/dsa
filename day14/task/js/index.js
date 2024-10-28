34d48c8c8dccf51f33fa66622f28e32daecd7961
Ishaku Dyelshak <ishakudyelshak@gmail.com>
2024-10-28 13:49:19 +0100

    Add files via upload

---
+++ day14/task/js/index.js
@@ -0,0 +1,4 @@
+let myname = "Dyelshak"
+let age = 22
+
+console.log(myname.slice(1,4));
---
+++ day14/task/js2/index.js
@@ -0,0 +1,64 @@
+// let myname = "Dyelshak"
+// let age = 22
+
+// console.log(age);
+// console.log(myname.length);
+
+// let username = myname.slice(0.4) + age
+
+
+// let= Text.myname.concat( "is" + hisname + " i am " + age + "years old" + " ia am a male" + "from" Nationality )
+// console.log(" my name is" + hisname + "i/,m" + age + "years old. i am a" + gender + "i/m from"+ nationality )
+
+// arreys are list of data
+
+let allstudents = [ 
+{
+    name:"mark",
+    nationality:"nigeria",
+    age: 22,
+    gender:"female",
+    score: 7,
+    outfit:{
+        skirt: "black",
+        shirt:"white"
+    }
+},
+{
+    name: "praise",
+    nationality:"nigeria",
+    age: 22,
+    gender:"female",
+    score: 5
+},
+{
+    name: "rejoice",
+    nationality:"nigeria",
+    age: 22,
+    gender:"female",
+    score: 10
+},
+{
+    name: "John",
+    nationality:"nigeria",
+    age: 22,
+    gender:"female",
+    score: 0
+},
+{
+    name: "steph",
+    nationality:"nigeria",
+    age: 22,
+    gender:"female",
+    score: 0
+}
+
+]
+let a=allstudents[0].score+20
+let b=allstudents[1].score+20
+let c=allstudents[2].score+20
+let d=allstudents[3].score+20
+let e=allstudents[4].score+20
+
+let totalscore=a+b+c+d+e
+console.log(totalscore)
